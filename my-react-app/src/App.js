70eb72bef532fc35a4652230aaf8cc29537138f9
SamBen16 <adresse123456789101112@gmail.com>
2024-05-19 14:00:57 +0200

    Added dynamic parameter :id to home page route

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -12,7 +12,7 @@ function App() {
        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/apartment" element={<Apartment />} />
+          <Route path="/apartment/:id" element={<Apartment />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
