eec7ab43121108696675627a3e43c5478dfbca5c
SamBen16 <adresse123456789101112@gmail.com>
2024-05-19 12:23:19 +0200

    creation of the Header component and use of the Link component of React Router

--- my-react-app/src/pages/Home.js
+++ my-react-app/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Home() {
     return (
         <div>
-            <p>Home</p>
+            <Header />
         </div>
-);
+    );
 }
 
 export default Home;
