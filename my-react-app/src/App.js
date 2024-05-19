0d509d0c9e0d8997f8200a4caff446f923b23dfe
SamBen16 <adresse123456789101112@gmail.com>
2024-05-19 12:51:58 +0200

    make style of Header and Footer component in mobile and desktop

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,7 +4,6 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Apartment from './pages/Apartment';
 import NotFound from './pages/NotFound';
-import './App.scss';
 
 function App() {
   return (
