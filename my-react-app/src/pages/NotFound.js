1712045fa0ae4dd2d3a7605f6a70822262938e96
SamBen16 <adresse123456789101112@gmail.com>
2024-05-19 18:23:21 +0200

    make NotFound page +updating App.js +make style

--- my-react-app/src/pages/NotFound.js
+++ my-react-app/src/pages/NotFound.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import pageErreur from '../assets/404.png'; 
+import retourPageDaccueil from '../assets/retourPageDaccueil.png'; 
 
 function NotFound() {
-    return (
+  return (
+    <div>
+      <Header />
+      <div className="style-pageErreur">
+        <img src={pageErreur} alt="page 404" id="page_404" />
+        <p className="oupsErrorPc">Oups! La page que vous demandez n'existe pas.</p>
+        <p className="oupsErrorMobile">Oups! La page que<br></br> vous demandez n'existe pas.</p>
         <div>
-            <p>Not found</p>
+          <Link to="/"><img src={retourPageDaccueil} alt="retour page d'accueil" id="retour_page_accueil" /></Link>
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default NotFound;
