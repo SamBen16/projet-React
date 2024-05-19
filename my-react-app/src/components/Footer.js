f3b61481c69ababe728fc52067e6c20b00af2b10
SamBen16 <adresse123456789101112@gmail.com>
2024-05-19 12:30:27 +0200

    creation of the Footer component and update of the Home component

---
+++ my-react-app/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LOGO from '../assets/LOGO.png';
+
+function Footer() {
+	return (
+        <div className="style-footer">
+            <div className="logo_footer">
+                <img src={LOGO} alt='logo KASA' />
+            </div>
+            <div className="copyRight_footer">
+                <div className="banniereCopyRight">© 2020 Kasa. All rights reserved</div>
+            </div>
+        </div>
+    );	
+}
+export default Footer
