e7a3a0af8e0db98c238350a55149d924a7291ad0
SamBen16 <adresse123456789101112@gmail.com>
2024-05-19 13:28:59 +0200

    creation HomeSubHeaderSection component + updating the Home page +make style

---
+++ my-react-app/src/components/HomeSubHeaderSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomeSubHeaderSection() {
+	return (
+		<div className='style_section'>
+            <p id="messageChezVous">Chez vous,&nbsp; </p>
+            <p id="messagePartout"> partout et ailleurs</p>
+
+		</div>
+	)
+}
+export default HomeSubHeaderSection;
--- my-react-app/src/pages/Home.js
+++ my-react-app/src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import HomeSubHeaderSection from '../components/HomeSubHeaderSection';
 
 function Home() {
     return (
         <div>
             <Header />
-
+            <div>
+            <HomeSubHeaderSection />
+            </div>
             <Footer />
         </div>
     );
