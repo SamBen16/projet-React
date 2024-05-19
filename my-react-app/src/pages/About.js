4ce51b1559287ea774b3baf892d3992875b1bf8e
SamBen16 <adresse123456789101112@gmail.com>
2024-05-19 14:11:04 +0200

    make About component without collapse

--- my-react-app/src/pages/About.js
+++ my-react-app/src/pages/About.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import aPropos from '../assets/aPropos.png';
 
 function About() {
-    return (
-        <div>
-            <p>About</p>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <div
+        className='style_section'
+        style={{
+          width: '95%',
+          height: '30vh',
+          backgroundImage: `url(${aPropos})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          opacity: 0.9,
+          filter: 'brightness(0.5)',
+        }}
+      ></div>
+      <Footer />
+    </div>
+  );
 }
 
 export default About;
