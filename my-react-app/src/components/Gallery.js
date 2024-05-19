8afece23588adea1db44ead88e8fe08ebff31ef2
SamBen16 <adresse123456789101112@gmail.com>
2024-05-19 13:55:07 +0200

    creation Gallery component +updating the Home page +make style

---
+++ my-react-app/src/components/Gallery.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { dataList } from '../data/data';
+
+function Gallery() {
+    return (
+        <div id="thumb-container">
+            <div className="thumb-container">
+                {dataList.map(item =>(
+                    <Link to={`/apartment/${item.id}`} key={item.id}>
+                        <div className="thumb" style={{ 
+                            backgroundImage: `linear-gradient(to top, #000, #0004 40%, #0000 65%, #0000 75%), url(${item.cover})`, 
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            }}>
+                            <p>{item.title}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Gallery;
--- my-react-app/src/pages/Home.js
+++ my-react-app/src/pages/Home.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import HomeSubHeaderSection from '../components/HomeSubHeaderSection';
+import Gallery from '../components/Gallery';
 
 function Home() {
     return (
         <div>
             <Header />
-            <div>
-            <HomeSubHeaderSection />
-            </div>
+                <div>
+                    <HomeSubHeaderSection />
+                    <div className="bodyGallery">
+                        <Gallery />
+                    </div>
+                </div>
             <Footer />
         </div>
     );
